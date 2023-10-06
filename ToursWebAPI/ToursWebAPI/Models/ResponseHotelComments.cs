8cddd80098e644842fe1a1c87f47726482a00f6e
Max <maxkorakov@gmail.com>
2023-10-06 11:17:06 +0500

    API

---
+++ ToursWebAPI/ToursWebAPI/Models/ResponseHotelComments.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ToursWebAPI.Entities;
+
+namespace ToursWebAPI.Models
+{
+    public class ResponseHotelComments
+    {
+        public ResponseHotelComments(HotelComment hotel)
+        {
+            Id = hotel.Id;
+            Text = hotel.Text;
+            HotelId = hotel.HotelId;
+            Author = hotel.Author;
+            CreationDate = hotel.CreationDate;
+        }
+
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public int HotelId { get; set; }
+        public string Author { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
